6a5fa1d5dd57af591f9992ffbf8d49499e998d4b
Dan Buch <daniel.buch@gmail.com>
2011-07-04 22:53:41 -0400

    reintroducing the script interface because that was just a bad idea w.r.t. reintegrating with mainline dev

--- pywordpress.py
+++ pywordpress.py
@@ -1,8 +1,14 @@
+#!/usr/bin/env python
+
+import inspect
+import optparse
+import pprint
+import sys
 import xmlrpc.client
 
 
 class Wordpress(object):
-    """Interact with an existing wordpress install via xml-rpc
+    '''Interact with an existing wordpress install via xml-rpc
 
     Sample usage::
 
@@ -15,7 +21,7 @@ class Wordpress(object):
         >>> print new_page['title']
         Title
         >>> 
-    """
+    '''
 
     def __init__(self, wp_url, username, password, blog_id=0, verbose=False):
         self.user = username
@@ -28,10 +34,10 @@ class Wordpress(object):
 
     @classmethod
     def init_from_config(self, config_fp):
-        """Class method to initialize a `Wordpress` instance from an ini file.
+        '''Class method to initialize a `Wordpress` instance from an ini file.
 
         For an example ini file see config.ini.tmpl
-        """
+        '''
         from configparser import ConfigParser
         cfg = ConfigParser()
         with open(config_fp) as infile:
@@ -47,7 +53,7 @@ class Wordpress(object):
             print(msg)
 
     def get_page_list(self):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.getPageList"""
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.getPageList'''
         results = self.server.wp.getPageList(
             self.blog_id,
             self.user,
@@ -56,7 +62,7 @@ class Wordpress(object):
         return results
 
     def get_page(self, page_id):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.getPage"""
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.getPage'''
         results = self.server.wp.getPage(
             self.blog_id,
             page_id,
@@ -66,7 +72,7 @@ class Wordpress(object):
         return results
 
     def get_pages(self):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.getPages"""
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.getPages'''
         results = self.server.wp.getPages(
             self.blog_id,
             self.user,
@@ -102,11 +108,11 @@ class Wordpress(object):
         return results
 
     def new_page(self, **kwargs):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.newPage
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.newPage
         
         :param **kwargs: all other possible arguments for content struct (see
         WP docs).
-        """
+        '''
         content_struct = dict(kwargs)
         if not 'mt_allow_comments' in content_struct:
             content_struct['mt_allow_comments'] = 0
@@ -125,7 +131,7 @@ class Wordpress(object):
         return page_id
         
     def delete_page(self, page_id):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.deletePage"""
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.deletePage'''
         result = self.server.wp.deletePage(
             self.blog_id,
             self.user,
@@ -135,20 +141,19 @@ class Wordpress(object):
         return bool(result)
 
     def delete_all_pages(self):
-        """Delete all pages (i.e. delete_page for each page in instance).
-        """
+        '''Delete all pages (i.e. delete_page for each page in instance).
+        '''
         for pagedict in self.get_page_list():
             self._print('Deleting: %s' % pagedict)
             self.delete_page(pagedict['page_id'])
 
     def edit_page(self, page_id, **kwargs):
-        """http://codex.wordpress.org/XML-RPC_wp#wp.editPage
+        '''http://codex.wordpress.org/XML-RPC_wp#wp.editPage
 
         Note: editing a page leads to a new revision.
 
-        :param **kwargs: attribute values for content struct (see wordpress
-                         docs).
-        """
+        :param **kwargs: attribute values for content struct (see wordpress docs).
+        '''
         # existing values not in content_struct are *not* left alone but are
         # set to empty string!
         edit_struct = self.get_page(page_id)
@@ -163,8 +168,7 @@ class Wordpress(object):
         return bool(result)
 
     def create_many_pages(self, pages_dict):
-        """ Create many pages at once (and only create pages which do not
-        already exist).
+        '''Create many pages at once (and only create pages which do not already exist).
 
         pages_dict = {
             '/about/': {
@@ -176,22 +180,21 @@ class Wordpress(object):
                 'description': 'xxx'
             }
         }
-        """
+        '''
         ## get a list of existing pages keyed by their urls
 
         ## use get_page_list as that shows trash items as well as active
-        pagelist = [self.get_page(x['page_id']) for x in self.get_page_list()]
+        pagelist = [ self.get_page(x['page_id']) for x in self.get_page_list()
+                ]
         pagedict = dict([ (p['page_id'], p) for p in pagelist ])
-
         def get_page_url(page):
             parent_id = page['wp_page_parent_id']
             if parent_id == 0:
                 return page['wp_slug']
             else:
-                return get_page_url(pagedict[parent_id]) + \
-                        '/' + page['wp_slug']
+                return get_page_url(pagedict[parent_id]) + '/' + page['wp_slug']
         existing_pages = dict(
-            [(get_page_url(p), p) for p in pagelist]
+                [(get_page_url(p), p) for p in pagelist]
         )
         changes = []
         # sort by key (url_path) so we can create in right order
@@ -220,3 +223,46 @@ class Wordpress(object):
                 changes.append([url_path, page_id, 'edited'])
         return changes
 
+
+def _object_methods(obj):
+    methods = inspect.getmembers(obj, inspect.ismethod)
+    methods = [name_y for name_y in methods if not name_y[0].startswith('_')]
+    methods = dict(methods)
+    return methods
+
+
+def main(sysargs=sys.argv[:]):
+    _methods = _object_methods(Wordpress)
+    usage = '''%prog {action}
+
+Actions:
+
+    '''
+    wpusage = '\n    '.join([
+        '%s: %s' % (name, m.__doc__.split('\n')[0] if m.__doc__ else '')
+        for (name, m) in sorted(_methods.items())
+    ])
+    usage += wpusage
+
+    parser = optparse.OptionParser(usage)
+    parser.add_option('-c', '--config',
+            help='configuration file to use (e.g. for wordpress config)',
+            default='config.ini')
+    options, args = parser.parse_args(sysargs[1:])
+
+    if not args or not args[0] in _methods:
+        parser.print_help()
+        return 1
+
+    action = args[0] 
+    wordpress = Wordpress.init_from_config(options.config)
+    wordpress.verbose = True
+    out = getattr(wordpress, action)(*args[1:])
+    if out:
+        pprint.pprint(out)
+
+    return 0
+
+
+if __name__ == '__main__':
+    sys.exit(main())
